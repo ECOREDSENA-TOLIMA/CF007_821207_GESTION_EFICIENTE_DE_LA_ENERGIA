2b4a08a464ade6a112701a405c9ca05d0f38d25b
Oscar Espitia <odespitia@sena.edu.co>
2023-07-10 22:06:28 -0500

    creditos

--- src/config/global.js
+++ src/config/global.js
@@ -311,6 +311,11 @@ export default {
       },
       {
         icono: 'fas fa-download',
+        titulo: 'Descargar PDF',
+        download: 'downloads/CFA_007.pdf',
+      },
+      {
+        icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
       },
@@ -323,32 +328,186 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Termodinámica',
+      referencia:
+        'Martín, A. (2015). Apuntes de los temas de termodinámica. Universidad Politécnica de Madrid.',
+      tipo: 'Página web',
+      link: 'https://oa.upm.es/38735/1/amd-apuntes-termodinamica-v3_0.pdf',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Hidrógeno verde',
+      referencia:
+        'Smink, V. (2021). Hidrógeno verde: 6  países que lideran la producción de una de las "energías del futuro" (y cuál es el único latinoamericano). BBC News.',
+      tipo: 'Página web',
+      link: 'https://www.bbc.com/mundo/noticias-56531777',
+    },
+    {
+      tema: 'Hidrógeno verde',
+      referencia:
+        'ProColombia. (2021). Hidrógeno verde, una alternativa para el futuro de la energía en Colombia.',
+      tipo: 'Página web',
+      link:
+        'https://investincolombia.com.co/es/articulos-y-herramientas/articulos/hidrogeno-verde-una-alternativa-para-el-futuro-de-la-energia-en-colombia',
+    },
+    {
+      tema: 'Huella de carbono',
+      referencia:
+        'IDEAM, PNUD. (2015). Cartilla Inventario Nacional de Gases de Efecto Invernadero (GEI) de Colombia. Tercera comunicación nacional del cambio climático de Colombia.',
+      tipo: 'Cartilla',
+      link:
+        'http://documentacion.ideam.gov.co/openbiblio/bvirtual/023421/cartilla_INGEI.pdf',
+    },
+    {
+      tema: 'Regulación de los servicios públicos domiciliarios',
+      referencia:
+        'Resolución 156 de 2011. [Comisión de Regulación de Energía y Gas]. Por la cual se establece el Reglamento de comercialización del servicio público de energía eléctrica, como parte del Reglamento de operación. Noviembre 17 de 2011.',
+      tipo: 'Documento legal',
+      link:
+        'https://www.enel.com.co/content/dam/enel-co/espa%C3%B1ol/2-empresas/2-2-emgesa/2-2-9-link-utiles/Creg_156_2011.pdf',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'ANH',
+      significado:
+        'Acrónimo que es utilizado para denotar la Agencia Nacional de Hidrocarburos.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'CRA',
+      significado:
+        'Acrónimo que es utilizado para denotar la Comisión de Regulación de Agua Potable y Saneamiento Básico.',
+    },
+    {
+      termino: 'DNP',
+      significado:
+        'Acrónimo que es utilizado para denotar el Departamento Nacional de Planeación.',
+    },
+    {
+      termino: 'IDEAM',
+      significado:
+        'Acrónimo que es utilizado para denotar el Instituto de Hidrología, Meteorología y Estudios Ambientales.',
+    },
+    {
+      termino: 'PARATEC',
+      significado:
+        'Acrónimo que es utilizado para denotar la página web propiedad de XM con la información de Parámetros Técnicos del Sistema Interconectado Nacional.',
+    },
+    {
+      termino: 'PNUD',
+      significado:
+        'Acrónimo que es utilizado para denotar el Programa de las Naciones Unidas para el Desarrollo.',
+    },
+    {
+      termino: 'Superservicios',
+      significado:
+        'Acrónimo que es utilizado para denominar a la Superintendencia de Servicios Públicos Domiciliarios.',
+    },
+    {
+      termino: 'TGI',
+      significado:
+        'Acrónimo que es utilizado para denotar a la Transportadora de Gas Internacional.',
+    },
+    {
+      termino: 'UPME',
+      significado:
+        'Acrónimo que es utilizado para denotar a la Unidad de Planeación Minero-Energética.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Agencia Nacional de Hidrocarburos ANH (2021). Datos y estadísticas. ',
+      link: 'https://www.anh.gov.co/datos-estadisticas',
+    },
+    {
+      referencia:
+        'Cengel, Y. & Boles, M. (2015). Termodinámica (Octava ed.). McGraw-Hill.',
       link: '',
+    },
+    {
+      referencia:
+        'Corporación Ambiental Empresarial. (2015). Factores de emisión considerados en la herramienta de cálculo de la huella de carbono corporativa. Versión 4. ',
+      link:
+        'https://www.acueducto.com.co/wps/html/resources/2018ag/huella_carbono/informe_gei/6_anexo_3Factores_Emision_Herramienta_Inventario_GEI_EAB_2014.pdf',
+    },
+    {
+      referencia:
+        'International Institute for Sustainable Development. (2021). EIA: 7 pasos. ',
+      link: 'https://www.iisd.org/learning/eia/es/eia-7-steps/',
+    },
+    {
+      referencia:
+        'International Renewable Energy Agency. (2020). Solar energy. IRENA ',
+      link:
+        'https://www.irena.org/Statistics/View-Data-by-Topic/Capacity-and-Generation/Technologies',
+    },
+    {
+      referencia:
+        'IPPC. (2014).  Fuentes de energía renovables y mitigación del cambio climático Informe Especial del GRUPO Intergubernamental de expertos sobre el Cambio Climático Resumen para responsables de políticas y Resumen técnico. ',
+      link:
+        'https://archive.ipcc.ch/pdf/special-reports/srren/srren_report_es.pdf',
+    },
+    {
+      referencia:
+        'Ley 142 de 1994. Por la cual se establece el régimen de los servicios públicos domiciliarios y se dictan otras disposiciones. Julio 11 de 1994. DO. No. 41.433. ',
+      link:
+        'http://www.secretariasenado.gov.co/senado/basedoc/ley_0142_1994.html',
+    },
+    {
+      referencia:
+        'Ley 143 de 1994. Por la cual se establece el régimen para la generación, interconexión, transmisión, distribución y comercialización de electricidad en el territorio nacional, se conceden unas autorizaciones y se dictan otras disposiciones en materia energética. Julio 12 de 1994. DO. No. 41.434. ',
+      link:
+        'http://www.secretariasenado.gov.co/senado/basedoc/ley_0143_1994.html',
+    },
+    {
+      referencia:
+        'Ministerio de Ambiente y Desarrollo Sostenible. (2020). Listado de impactos ambientales específicos en el Marco del Licenciamiento Ambiental. ',
+      link:
+        'http://www.andi.com.co/Uploads/listado-de-impactos-ambientales-espec%C3%ADficos-en-el-marco-del-licenciamiento-ambiental.pdf',
+    },
+    {
+      referencia:
+        'Ministerio de Minas y Energía. (2018). Boletín estadístico de Minas y Energía 2018. ',
+      link:
+        'https://www1.upme.gov.co/PromocionSector/SeccionesInteres/Documents/Boletines/Boletin_Estadistico_2018.pdf',
+    },
+    {
+      referencia: 'PARATEC. (2021). Parámetros técnicos del SIN. XM. ',
+      link: 'http://paratec.xm.com.co/paratec/SitePages/default.aspx',
+    },
+    {
+      referencia:
+        'Sistema de Información Minero Colombiano. (2021). Carbón-Producción. de ',
+      link:
+        'https://www1.upme.gov.co/simco/Cifras-Sectoriales/Paginas/carbon.aspx',
+    },
+    {
+      referencia:
+        'Superintendencia de Servicios Públicos. (2015). Superservicios para todos. Imprenta Nacional de Colombia. ',
+      link:
+        'https://www.superservicios.gov.co/sites/default/archivos/SSPD%20Publicaciones/Publicaciones/2018/Oct/superservicios_para_todos.pdf',
+    },
+    {
+      referencia: 'TGI. (2021). ¿Cómo llega el gas a mi casa o negocio? ',
+      link:
+        'https://www.tgi.com.co/industria-del-gas-natural/como-llega-el-gas-a-mi-casa-o-negocio',
+    },
+    {
+      referencia:
+        'Unidad de Planeación Minero-Energética. (2004). Una visión del mercado eléctrico colombiano. UPME ',
+      link:
+        'http://www.upme.gov.co/Docs/Vision_Mercado_Electrico_Colombiano.pdf',
+    },
+    {
+      referencia:
+        'Unidad de Planeación Minero-Energética. (2019). Primer balance de energía útil para Colombia. UPME ',
+      link:
+        'https://www1.upme.gov.co/Hemeroteca/Paginas/estudio-primer-balance-energia-util-para-Colombia.aspx',
+    },
+    {
+      referencia:
+        'XM. (2020). Reporte integral de sostenibilidad, operación y mercado 2020. XM. ',
+      link: 'https://informeanual.xm.com.co/2020/informe/pages/home.html',
     },
   ],
   creditos: [
@@ -361,9 +520,9 @@ export default {
           centro: 'Dirección General',
         },
         {
-          nombre: 'Nombre completo',
+          nombre: 'Norma Constanza Morales Cruz',
           cargo: 'Responsable de línea de producción',
-          centro: 'Nombre centro de formación',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
@@ -371,9 +530,45 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Nombre centro de formación',
+          nombre: 'Marlon Augusto Villamizar Morales',
+          cargo: 'Experto Técnico',
+          centro: 'Global Green Growth Institute (GGGI)',
+        },
+        {
+          nombre: 'Linda Díaz Rivera',
+          cargo: 'Consultora',
+          centro: 'Global Green Growth Institute (GGGI)',
+        },
+        {
+          nombre: 'Carolina Arias',
+          cargo: 'Diseñadora Instruccional',
+          centro: 'Regional Distrito Capital - Centro de diseño y metrología',
+        },
+        {
+          nombre: 'Carolina Coca Salazar',
+          cargo: 'Revisora Metodológica y Pedagógica',
+          centro: 'Regional Distrito Capital - Centro de Diseño y Metrología',
+        },
+        {
+          nombre: 'Rafael Neftalí Lizcano Reyes',
+          cargo: 'Responsable Equipo Desarrollo Curricular',
+          centro:
+            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+        },
+        {
+          nombre: 'Julia Isabel Roberto',
+          cargo: 'Corrector de Estilo',
+          centro: 'Regional Distrito Capital - Centro de Diseño y Metrología',
+        },
+        {
+          nombre: 'Juan Gilberto Giraldo Cortés',
+          cargo: 'Diseñador Instruccional',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'María Inés Machado López',
+          cargo: 'Metodóloga',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
@@ -381,19 +576,34 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador web',
-          centro: 'Nombre centro de formación',
+          nombre: 'José Yobani Penagos Mora',
+          cargo: 'Diseñador Web',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Oscar Daniel Espitia Marin',
           cargo: 'Desarrollador Fullstack',
-          centro: 'Nombre centro de formación',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
         {
-          nombre: 'Nombre',
-          cargo: 'Animador y Producción audiovisual',
-          centro: 'Nombre centro de formación',
+          nombre: 'Gilberto Junior Rodríguez Rodríguez',
+          cargo: 'Storyboard e Ilustración',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'Nelson Iván Vera Briceño',
+          cargo: 'Producción Audiovisual',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'Oleg Litvin',
+          cargo: 'Animador',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'Francisco Javier Vásquez Suarez',
+          cargo: 'Actividad Didáctica',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
@@ -401,14 +611,14 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Nombre validador',
+          nombre: 'Jorge Bustos Gómez',
           cargo: 'Validación y vinculación en plataforma LMS',
-          centro: 'Nombre centro formación',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
         {
-          nombre: 'Nombre validador',
+          nombre: 'Gilberto Naranjo Farfán',
           cargo: 'Validación de contenidos accesibles',
-          centro: 'Nombre centro formación',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
